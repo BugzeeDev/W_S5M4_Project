972015f6016a4e2ed93ecadbddb632d446305812
BugzeeDev <irvingcsylva@gmail.com>
2024-08-05 03:02:01 -0700

    fleshed out basic API and functions

--- frontend/index.js
+++ frontend/index.js
@@ -15,7 +15,29 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
+  
+  document.querySelector('#weatherWidget').style.display = 'none'
+  const dropDown = document.querySelector("#citySelect")
+  dropDown.addEventListener('change', async evt => {
+    console.log('selection changed')
+    try {
+      evt.target.setAttribute('disabled', 'disabled')
+      document.querySelector('#weatherWidget').style.display = 'none'
+      document.querySelector('.info').textContent = 'Fetching weather data...'
 
+      console.log(evt.target.value)
+      let city = evt.target.value
+      let url = `http://localhost:3003/api/weather?city=${city}`
+      const result = await axios.get(url)
+      
+      document.querySelector('#weatherWidget').style.display = 'block'
+      document.querySelector('.info').textContent = ''
+      evt.target.removeAttribute('disabled')
+
+    } catch (error) {
+      console.log(`Promise rejected with an error: `. error.message)
+    }
+  })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 }
